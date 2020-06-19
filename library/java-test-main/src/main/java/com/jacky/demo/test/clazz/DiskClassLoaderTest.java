f1813c788271a82f898dc7d6bdf5bca46265d876
杨剑飞 <847564732@qq.com>
2020-06-20 00:06:44 +0800

    custom classloader

--- library/java-test-main/src/main/java/com/jacky/demo/test/clazz/DiskClassLoaderTest.java
+++ library/java-test-main/src/main/java/com/jacky/demo/test/clazz/DiskClassLoaderTest.java
@@ -1,5 +1,12 @@
 package com.jacky.demo.test.clazz;
 
+import com.jacky.tool.util.Util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 /**
  * Created by Jacky on 2020/6/19
  * <p>
@@ -7,10 +14,56 @@ package com.jacky.demo.test.clazz;
  */
 public class DiskClassLoaderTest {
     void start() {
+        final DiskClassLoader diskClassLoader = new DiskClassLoader("D://");
+        ClassLoader entry = diskClassLoader;
 
+        while (entry != null) {
+            Util.i("classLoader: %s", entry);
+            entry = entry.getParent();
+        }
     }
 
     public static void main(String[] args) {
         new DiskClassLoaderTest().start();
+    }
+
+    private static class DiskClassLoader extends ClassLoader {
+        private final String mLibPath;
+
+        public DiskClassLoader(String mLibPath) {
+            this.mLibPath = mLibPath;
+        }
+
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            byte[] classData = getClassData(name);
+            if (classData == null) {
+                throw new ClassNotFoundException();
+            } else {
+                return defineClass(name, classData, 0, classData.length);
+            }
+        }
+
+        private byte[] getClassData(String className) {
+            String path = classNameToPath(className);
+            try {
+                InputStream ins = new FileInputStream(path);
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                int bufferSize = 4096;
+                byte[] buffer = new byte[bufferSize];
+                int bytesNumRead = 0;
+                while ((bytesNumRead = ins.read(buffer)) != -1) {
+                    baos.write(buffer, 0, bytesNumRead);
+                }
+                return baos.toByteArray();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        private String classNameToPath(String className) {
+            return mLibPath + File.separatorChar
+                + className.replace('.', File.separatorChar) + ".class";
+        }
     }
 }
